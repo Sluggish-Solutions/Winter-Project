3680178fdc57d0c9c48436ae6936a18213bc9942
suri312006 <suri312006@gmail.com>
2023-12-22 12:47:39 -0800

    image genreation from prompt works, PLEASE DONT SEND PROMPTS, IMA LOSE MONEY

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,21 @@
-import OpenAI from 'openai';
-import { OPENAI_API_KEY } from '$env/static/private';
+import OpenAI from 'openai'
+import { OPENAI_API_KEY } from '$env/static/private'
 
-const openai = new OpenAI({apiKey: OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
 
 export const actions = {
-	generateImage: async({request}: {request: any}) => {
-		console.log(await request.formData());
-		let promptRequest = await request.formData();
-		let prompt = promptRequest.get('imgPrompt') || "wtf";
+	generateImage: async ({ request }: { request: any }) => {
+		let promptRequest = await request.formData()
+		console.log(promptRequest)
+		let prompt = promptRequest.get('imgPrompt')
 		const image = await openai.images.generate({
 			prompt: prompt.toString(),
 			response_format: 'b64_json'
-		});	
-
-		// * wtf do i do brooooo???
+		})
 
 		return {
-			b64: image.data,
-		};
+			prompt: prompt,
+			b64: image.data[0].b64_json
+		}
 	}
-};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,21 @@
 <script lang="ts">
-	let imgPrompt = ''
-
-	let b64_img: string
-
+	export let form
 </script>
 
 <main class="flex flex-col justify-center items-center p-3 sm:p-10">
+	{#if form?.b64}
+		<h1>here is your image</h1>
+
+		<img src="data:image/png;base64,{form.b64}" alt="generated img" />
+		<h1>{form.prompt}</h1>
+	{:else}
 	<!-- Image Placeholder-->
-	<img
-		src="https://images.unsplash.com/photo-1682685796186-1bb4a5655653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-		alt=""
-	/>
-
-	<!-- Form for Image Prompt -->
-
-<!-- 	<form action='?/generateImage' method="POST" class="flex flex-col w-full my-3">
-		<label class="label"
-			<span>Image Prompt</span>
-			<input
-				class="textarea resize-none"
-				placeholder="Superman's cat lying on the moon."
-			/>
-		</label>
-
-		<button type="submit" class="btn variant-filled">Submit</button>
-
-	</form> -->
+		<img
+			src="https://images.unsplash.com/photo-1682685796186-1bb4a5655653?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt=""
+		/>
+	{/if}
 	<form method="POST" action="?/generateImage">
-		
 		<label class="label">
 			<span>Image Prompt</span>
 			<textarea
@@ -39,7 +27,5 @@
 		</label>
 
 		<button type="submit" class="btn variant-filled">Submit</button>
-
-
 	</form>
 </main>
