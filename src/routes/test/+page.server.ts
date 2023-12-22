48fd139550340643cb83c2f202848a67230efeff
suri312006 <suri312006@gmail.com>
2023-12-21 20:40:46 -0800

    using b64 data from open ai now, DONT OPEN TESTING :SOB:

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,15 +1,15 @@
 import OpenAI from 'openai';
-import type { PageLoad } from './$types'
 import { OPENAI_API_KEY } from '$env/static/private';
 
 const openai = new OpenAI({apiKey: OPENAI_API_KEY})
 export const load = (async () => {
 	const image = await openai.images.generate({
-		prompt: 'cat',
+		prompt: 'cat', 
+		response_format:'b64_json'
 	})
 
 	console.log(image)
 	  
-	return {url: image.data[0].url}
+	return {base64: image.data[0].b64_json}
 	//return {url: image.data.url}
-}) satisfies PageLoad
+}) 
