7331c146157b8f8c5993edfbeae3c92748208dfc
Kenric Tee <kenrictee@Kenrics-MacBook-Air.local>
2023-12-21 19:59:47 -0800

    form

---
+++ src/routes/Form.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let imgPrompt = '';
+
+  function handleSubmit() {
+    // send to dalle to create image
+    console.log(`Submitted: ${imgPrompt}`);
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="flex flex-col">
+  <label for="name" class="h3">Image Prompt</label>
+  <input id="email" bind:value={imgPrompt} type="email" required class="mb-4 h-32" />
+
+  <button type="submit" class=" variant-filled-primary">Submit</button>
+</form>
